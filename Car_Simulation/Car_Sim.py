6408863798bc6b7b614a64ce919becbdee963fbc
Aldernine <ehutch924@gmail.com>
2022-02-08 18:48:18 -0500

    updates

--- Car_Simulation/Car_Sim.py
+++ Car_Simulation/Car_Sim.py
@@ -1,16 +1,26 @@
 choice = ''
 
+started = False
+
 while choice != 'QUIT':
     choice = input('')
     if choice.upper() == 'START':
-        print("Car started. Ready to go!")
+        if started:
+            print("Car is already started.")
+        else:
+            started = True
+            print("Car started. Ready to go!")
     elif choice.upper() == 'STOP':
-        print('Car stopped')
+        if not started:
+            print("Car is already stopped.")
+        else:
+            started = False
+            print('Car stopped')
     elif choice.upper() == 'QUIT':
         break
     elif choice.upper() == 'HELP':
-        print("Start - Start car."
-              "Stop - Stop Car"
-              "Quit - Quit")
+        print('''Start - Start car.
+Stop - Stop Car.
+Quit - Quit.''')
     else:
         print("I don't understand")
